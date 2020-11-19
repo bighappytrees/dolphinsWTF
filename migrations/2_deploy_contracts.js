e81aec45e0420167bd518f161c66970af9663197
cetacean Queen <cetaceanqueen@protonmail.ch>
2020-11-19 17:49:19 +0100

    fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,41 @@
 const EEEE = artifacts.require("eeee");
-const DolphinPod = artifacts.require("DolphinPod");
+const DolphinPod1 = artifacts.require("DolphinPod");
+const DolphinPod2 = artifacts.require("DolphinPod");
+const snatchFeeder = artifacts.require("snatchFeeder");
 
-const eeeeToFarm = web3.utils.toBN('21000000000000000000000');
 
-const eeeePerBlock = web3.utils.toBN('500000000000000000');
-const durationBlocks = 42000;
-const minElapsedBlocksBeforeStart = 2;
-const startBlock = 14;
+const eeeeToFarm1 = web3.utils.toBN('21000000000000000000000');
+const eeeePerBlock1 = web3.utils.toBN('500000000000000000');
+const durationBlocks1 = web3.utils.toBN('42');
+const minElapsedBlocksBeforeStart1 = web3.utils.toBN('2');
 
+
+const eeeeToFarm2 = web3.utils.toBN('6969000000000000000000');
+const eeeePerBlock2 = web3.utils.toBN('500000000000000000');
+const durationBlocks2 = web3.utils.toBN('84');
+const minElapsedBlocksBeforeStart2 = web3.utils.toBN('2');
+
+
+const eeeeToSnatch = web3.utils.toBN('14100000000000000000000');
 
 module.exports = async (deployer) => {
     await deployer.deploy(EEEE);
     eeeeInstance = await EEEE.deployed();
-    //await deployer.deploy(DolphinPod, eeeeInstance.address, eeeePerBlock, durationBlocks, minElapsedBlocksBeforeStart, startBlock);
+
+    const startBlock1 = await web3.eth.getBlockNumber() + 10;
+    const offsetStart2 = (startBlock1 + durationBlocks1 + 10);
+    const startBlock2 = offsetStart2;
+
+    await deployer.deploy(DolphinPod1, eeeeInstance.address, eeeePerBlock1, durationBlocks1, minElapsedBlocksBeforeStart1, startBlock1);
+    pod1 = await DolphinPod1.deployed();
+    eeeeInstance.transfer(pod1.address, eeeeToFarm1);
+    
+    await deployer.deploy(DolphinPod2, eeeeInstance.address, eeeePerBlock2, durationBlocks2, minElapsedBlocksBeforeStart2, startBlock2);
+    pod2 = await DolphinPod2.deployed();
+    eeeeInstance.transfer(pod1.address, eeeeToFarm2);
+
+    await deployer.deploy(snatchFeeder, eeeeInstance.address);
+    snatchInstance = await snatchFeeder.deployed();
+    eeeeInstance.transfer(snatchInstance.address, eeeeToSnatch);
 };
 
